2f085418d9844421020c36231a075df6097501ae
beyonddream <43626691+beyonddream@users.noreply.github.com>
2023-10-22 14:43:57 -0700

    * add some basic logic (untested)

--- magic8ball.rb
+++ magic8ball.rb
@@ -2,8 +2,20 @@
 
 class Magic8Ball
 
+    def initialize(consistency=false, answers_file_path="./answers.txt")
+        @consistency = consistency
+        @answers = []
+        File.open(answers_file_path).each do |answer|
+            @answers.push(answer)
+        end
+    end
 
-
-    
-
+    def ask(question="")
+        return "You must ask a question!" unless question
+        if consistency
+            return answers[0]
+        else
+            return answers[rand(0..(answers.length()-1))]
+        end
+    end
 end
