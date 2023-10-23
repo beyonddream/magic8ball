54be55805236cb6eecfd7567a15ab1da45f78aa2
beyonddream <43626691+beyonddream@users.noreply.github.com>
2023-10-22 22:44:43 -0700

    * add doc comments

--- magic8ball.rb
+++ magic8ball.rb
@@ -1,7 +1,18 @@
 # frozen_string_literal: true
 
+##
+# This class represents the Magic 8 Ball.
+#
 class Magic8Ball
 
+    # Public: Setup magic 8 ball
+    #
+    # answers_file_path - path to a file which contains list of possible answers.
+    #   Defaults to the file within the app directory.
+    #
+    # consistency - If true, then same question will always yield same random answer. If not, it returns a random answer everytime even for same question.
+    #
+    # Returns nothing.
     def initialize(answers_file_path:File.join(File.dirname(__FILE__), 'answers.txt'), consistency:false)
         @answers = []
         File.open(answers_file_path).each do |answer|
@@ -10,6 +21,11 @@ class Magic8Ball
         @consistency = consistency
     end
 
+    # Public: main method to ask a question to magic 8 ball.
+    #
+    # question - question that is asked to magic 8 ball. Cannot be nil.
+    #
+    # Returns the answer
     def ask(question=nil)
         return "You must ask a question!" unless question
         if @consistency
